9c92e34b14061ac4bf3c3ea4b5d557ad59a4b87d
Vitor Araujo <vittor.sic@gmail.com>
2019-07-18 01:09:45 -0300

    feat: add validation to input component

--- app/components/olist-input/olist-input.js
+++ app/components/olist-input/olist-input.js
@@ -22,11 +22,20 @@ class OlistInputComponent extends HTMLElement {
         this.root.innerHTML = `
             <div class="olist-input-wrapper">
                 <label class="input-label">${this.getAttribute("label")}</label>
-                <input class="input-item " type="${this.getAttribute("type")}" ${this.placeholder}">
+                <input class="input-item" type="${this.getAttribute("type")}" ${this.placeholder}">
             </div>
         `;
         const style = componetStyle.cloneNode(true);
         this.root.append(style);
+        this.element = this.root.querySelector("input");
+
+        this.element.addEventListener('input', () => {
+            this.isValid = this.textValidation(this.type, this.value);
+            if (this.isValid)
+                this.classValidationManager("valid");
+            else
+                this.classValidationManager("invalid");
+        });
     }
 
     get value() {
@@ -39,6 +48,61 @@ class OlistInputComponent extends HTMLElement {
 
     get type() {
         return this.getAttribute("type");
+    }
+
+    passwordValidation(){
+        return {
+            sixChar: false,
+            oneUppercase: false,
+            oneNumber: false
+        }
+    }
+
+    textValidation(type) {
+        const validationFunctions = {
+            email: this.emailInputValidation,
+            name: this.nameInputValidation,
+            password: this.passwordInputValidation
+        }
+        // At this moment the methods lost the main reference, so I pass the context to them
+        return validationFunctions[type](this);
+    }
+
+    emailInputValidation(ctx) {
+        var emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        ctx.value = ctx.value.toLowerCase();
+        return emailRegex.test(String(ctx.value));
+    }
+
+    nameInputValidation(ctx) {
+        var nameRegex = /^([a-zA-z]+([ '-]([a-zA-Z]{3,})+){1,})$/;
+        return nameRegex.test(String(ctx.value));
+    }
+
+    passwordInputValidation(ctx) {
+        var sixCharRegex = /(.){6}/gm;
+        ctx.passwordValidation.sixChar = sixCharRegex.test(String(ctx.value));
+
+        var oneUppercaseRegex = /([A-Z])/gm;
+        ctx.passwordValidation.oneUppercase = oneUppercaseRegex.test(String(ctx.value));
+
+        var oneNumberRegex = /(\d)/gm;
+        ctx.passwordValidation.oneNumber = oneNumberRegex.test(String(ctx.value));
+
+        if (ctx.passwordValidation.sixChar && ctx.passwordValidation.oneUppercase && ctx.passwordValidation.oneNumber)
+            return true;
+
+        return false;
+    }
+
+    classValidationManager(className) {
+        if (className === "valid" && this.element.classList.contains('invalid'))
+           this.element.classList.remove('invalid');
+
+        if (className === "invalid" && this.element.classList.contains('valid'))
+            this.element.classList.remove('valid');
+
+        return this.element.classList.add(className);
     }
 
 }
